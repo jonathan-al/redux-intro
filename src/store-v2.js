25f40d8bf5d83cc38c03f2b289ae63a5702a48b3
Jonathan Alcantara <jonathan.alcantara@improving.com>
2024-03-11 20:37:59 -0600

    feat: versions of store

---
+++ src/store-v2.js
@@ -0,0 +1,19 @@
+import { composeWithDevTools } from "@redux-devtools/extension"
+import { applyMiddleware, combineReducers, createStore } from "redux"
+import { thunk } from "redux-thunk"
+import accountReducer from "./features/accounts/accountSlice"
+import customerReducer from "./features/customers/customerSlice"
+
+/* ######################################## */
+// Reducers
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+})
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk)),
+)
+
+export default store
